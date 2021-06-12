30e743a63207fa93725b8bbb38f05baaa8cba5cc
unknown <sameer9nov@gmail.com>
2021-06-12 23:11:13 +0000

    staticman test

--- themes/hugo-refresh/assets/js/refresh.js
+++ themes/hugo-refresh/assets/js/refresh.js
@@ -209,7 +209,13 @@ $(document).ready(function () {
         });
 })
 
-function changeValue(elementName, newValue){
-  document.getElementsByName(elementName)[0].value=newValue;
-  window.location.hash = "#comment-form";
-};
+
+function replyTo(parent, name) {
+  var e = document.getElementById(parent),
+      f = document.getElementById('comment-form'),
+      h = document.getElementById('comment-form-header');
+
+  h.innerHTML = 'Reply to ' + name;
+  e.parentNode.insertBefore(f, e.nextSibling);
+  document.getElementsByName('fields[reply_to]')[0].value=parent;
+}
